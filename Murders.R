5c69ef6270427e9c04d410422e7aaaa5c9ba700b
TemTee <temmyteeliz@gmail.com>
2022-03-10 13:41:06 +0100

    update

--- Murders.R
+++ Murders.R
@@ -4,9 +4,14 @@ library(dslabs)
 library(tidyverse)
 library(dplyr)
 data("murders")
+us_gun_murder <- save(murders, file = "murders.rda")
 
 
 # Exploring the data  ----------------------------------------------
 str(murders)
 head(murders)
-rename(murders, murder_total = total)
+table(is.na(murders))
+murders <- murders %>% mutate(rate = total/population *10^5)
+hist(murders$total, col = "cyan 3")
+hist(murders$rate, col = "cyan 3")
+plot(murders$population, murders$total, col = "red")
