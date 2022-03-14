2b8fa044ad3e1a304324f99d238df7abe78edeb7
TemTee <temmyteeliz@gmail.com>
2022-03-14 22:36:19 +0100

    update

--- Murders.R
+++ Murders.R
@@ -15,6 +15,12 @@ murders <- murders %>% mutate(rate = total/population *10^5)
 head(murders)
 hist(murders$total, col = "cyan 3")
 hist(murders$rate, col = "cyan 3")
-plot(murders$population, murders$total, col = "red")
+murders %>% mutate(pop= population /10^5) %>%
+  ggplot(aes(pop,total, color = region, label = state)) +
+  geom_text() +
+  geom_jitter(position = "jitter")
+  
 murders %>% mutate(pop= population /10^5) %>% 
-  ggplot(aes(pop,rate, color = region, label = abb))
+  ggplot(aes(pop,rate, color = region, label = state)) +
+  geom_text() +
+  geom_jitter(position = "jitter")
